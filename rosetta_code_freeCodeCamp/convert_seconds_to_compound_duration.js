3ec321616ef23788ae8ab4fac02f345d891ba73e
vaaghu <73047250+Vignesh021102@users.noreply.github.com>
2022-01-15 15:51:29 +0530

    Update convert_seconds_to_compound_duration.js

--- rosetta_code_freeCodeCamp/convert_seconds_to_compound_duration.js
+++ rosetta_code_freeCodeCamp/convert_seconds_to_compound_duration.js
@@ -1,58 +1,61 @@
+/*Implement a function which:
+
+takes a positive integer representing a duration in seconds as input (e.g., 100), and
+returns a string which shows the same duration decomposed into weeks, days, hours, minutes, and seconds as detailed below (e.g., 1 min, 40 sec).
+Demonstrate that it passes the following three test-cases:
+
+Test Cases
+Input number	Output number
+7259	        2 hr, 59 sec
+86400	        1 d
+6000000	        9 wk, 6 d, 10 hr, 40 min
+
+Details
+The following five units should be used: 
+Unit	Suffix used in Output	Conversion
+week	wk	                    1 week = 7 days
+day	    d	                    1 day = 24 hours
+hour	hr	                    1 hour = 60 minutes
+minute	min	                    1 minute = 60 seconds
+second	sec	                    ---
+However, only include quantities with non-zero values in the output (e.g., return 1 d and not 0 wk, 1 d, 0 hr, 0 min, 0 sec).
+Give larger units precedence over smaller ones as much as possible (e.g., return 2 min, 10 sec and not 1 min, 70 sec or 130 sec).
+Mimic the formatting shown in the test-cases (quantities sorted from largest unit to smallest and separated by comma+space; value and unit of each quantity separated by space). */
+
+
 function convertSeconds(s){
-    console.log(sec);
-    let year = sec/(12*4.34524*7*24*60*60);
-    let month = (year -Math.floor(year))*12;
-    let week = (month -Math.floor(month))*4.34524;
+    let week = s/(7*24*60*60);
     let day = (week -Math.floor(week))*7
     let hour = (day -Math.floor(day))*24
     let min = (hour-Math.floor(hour))*60;
-    sec = sec/60;
+    sec = s%60;
 
  
-    let str = "";
-    if(Math.floor(year) > 0){
-        str += ` ${Math.floor(year)} year,`
-    }
-    if(Math.floor(month) >0){
-        str += ` ${Math.floor(month)} months,`
-    }
+    let str = [];
+    
     if(Math.floor(week) >0){
-        str += ` ${Math.floor(week)} weeks,`
+        str.push(`${Math.floor(week)} wk`)
     }
     if(Math.floor(day) >0){
-        str += ` ${Math.floor(day)} days,`
+        str.push(`${Math.floor(day)} d`)
     }
     if(Math.floor(hour) >0){
-        str += ` ${Math.floor(hour)} hours,`
+        str.push(`${Math.floor(hour)} hr`)
     }
     if(Math.floor(min) >0){
-        str += ` ${Math.floor(min)} mins,`
+        str.push(`${Math.floor(min)} min`)
     }
     if(Math.floor(sec) >0){
-        str += ` ${Math.floor(sec)} seconds,`
+        str.push(`${Math.floor(sec)} sec`)
     }
+    str = str.join(", ");
+    str = str.trim();
     console.log(str);
+    return str
 }
 
 
 
-function getSeconds(sec,min,hour,day,week,month,year){
-    if(sec == undefined) return 0;
-    let result = sec;
-    if(min == undefined)return result;
-    result += (min*60);
-    if(hour == undefined)return result;
-    result += hour*(60*60);
-    if(day == undefined)return result;
-    result += day*(60*60*24);
-    if(week == undefined)return result;
-    result += week*(60*60*24*7);
-    if(month == undefined)return result;
-    result += month*(60*60*24*7*4.34524);
-    if(year == undefined)return year;
-    result += year*(60*60*24*7*4.34524*12);
-    return result;
-}
-let sec = getSeconds(60,1);
-console.log(sec);
-convertSeconds(sec);
+console.log(convertSeconds(7259))// should return 2 hr, 59 sec.
+convertSeconds(86400)// should return 1 d.
+convertSeconds(6000000)
